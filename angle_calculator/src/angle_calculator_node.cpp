1691308c5f24fd81329bb33f2554e4b755fec4b8
LukyKozlojc <l.kozel@outlook.cz>
2023-11-26 21:46:33 +0100

    fixed all frames, so the state vector should be correct. Still issues with polarization for angles and roll and pitch

--- angle_calculator/src/angle_calculator_node.cpp
+++ angle_calculator/src/angle_calculator_node.cpp
@@ -47,56 +47,61 @@ private:
         latest_clock_ = msg->clock;
     }
  //   }
-    void calculate_angles()
-    {
-        if (!load_pose_ || !drone_pose_) {
-            return;
-        }
-        tf2::Quaternion drone_orientation(
+void calculate_angles()
+{
+    if (!load_pose_ || !drone_pose_) {
+        return;
+    }
+
+    // Extract yaw from drone's orientation
+    tf2::Quaternion drone_orientation(
         drone_pose_->pose.orientation.x,
         drone_pose_->pose.orientation.y,
         drone_pose_->pose.orientation.z,
         drone_pose_->pose.orientation.w
-        );
-        tf2::Matrix3x3 drone_rotation_matrix(drone_orientation);
-        tf2::Matrix3x3 inverse_drone_rotation = drone_rotation_matrix.inverse();
-        tf2::Vector3 load_position(
+    );
+    double roll, pitch, yaw;
+    tf2::Matrix3x3(drone_orientation).getRPY(roll, pitch, yaw);
+
+    // Rotation matrix for rotation around Z-axis
+    tf2::Matrix3x3 rotation_matrix;
+    rotation_matrix.setRPY(0, 0, yaw);
+
+    // Load's position
+    tf2::Vector3 load_position(
         load_pose_->pose.position.x,
         load_pose_->pose.position.y,
         load_pose_->pose.position.z
-        );
-        tf2::Vector3 transformed_load_position = inverse_drone_rotation * load_position;
-        double x = transformed_load_position.x();
-        double y = transformed_load_position.y();
-        double z = transformed_load_position.z();
-        //double x = load_pose_->pose.position.x;
-        //double y = load_pose_->pose.position.y;
-        //double z = load_pose_->pose.position.z;
-        double projection_xy_magnitude = std::sqrt(x * x + y * y);
+    );
+    // Apply rotation to load's position
+    tf2::Vector3 rotated_load_position = rotation_matrix * load_position;
 
-        if(projection_xy_magnitude == 0) {
-            RCLCPP_WARN(this->get_logger(), "Projection onto xy-plane has zero magnitude, cannot calculate θ_z");
-            return;
-        }
+    double x = rotated_load_position.x();
+    double y = rotated_load_position.y();
+    double z = rotated_load_position.z();
+    RCLCPP_INFO(this->get_logger(), "x = %.2fy = %.2f z = %.2f",
+            x, y, z);
+    double projection_xy_magnitude = std::sqrt(x * x + y * y);
 
-        double theta_x_rad = std::atan2(z, x);
-        double theta_y_rad = std::atan2(z, y);
-        double theta_z_rad = std::atan2(projection_xy_magnitude, -z);
-
-        theta_x_rad = theta_x_rad + M_PI/2;
-        theta_y_rad = theta_y_rad + M_PI/2;
-        theta_z_rad = theta_z_rad;
-
-        RCLCPP_INFO(this->get_logger(), "Theta: θ_x = %.2f rad, θ_y = %.2f rad, θ_z = %.2f rad",
-                    theta_x_rad, theta_y_rad, theta_z_rad);
-
-        auto angle_msg = angle_stamped_msg::msg::AngleStamped();
-        angle_msg.header.stamp = latest_clock_;
-        angle_msg.angle.angle_x = theta_x_rad;
-        angle_msg.angle.angle_y = theta_y_rad;
-        angle_msg.angle.angle_z = theta_z_rad;
-        load_angle_publisher_->publish(angle_msg);
+    if (projection_xy_magnitude == 0) {
+        RCLCPP_WARN(this->get_logger(), "Projection onto xy-plane has zero magnitude, cannot calculate θ_z");
+        return;
     }
+
+    double theta_x_rad = std::atan2(z, x) + M_PI/2 - pitch;
+    double theta_y_rad = std::atan2(z, y) + M_PI/2 + roll;
+    double theta_z_rad = std::atan2(projection_xy_magnitude, -z);
+
+    RCLCPP_INFO(this->get_logger(), "Theta: θ_x = %.2f rad, θ_y = %.2f rad, θ_z = %.2f rad",
+                theta_x_rad, theta_y_rad, theta_z_rad);
+
+    auto angle_msg = angle_stamped_msg::msg::AngleStamped();
+    angle_msg.header.stamp = latest_clock_;
+    angle_msg.angle.angle_x = theta_x_rad;
+    angle_msg.angle.angle_y = theta_y_rad;
+    angle_msg.angle.angle_z = theta_z_rad;
+    load_angle_publisher_->publish(angle_msg);
+}
 
     rclcpp::Subscription<load_pose_stamped::msg::LoadPoseStamped>::SharedPtr load_pose_subscriber_;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr drone_pose_subscriber_;
@@ -114,3 +119,4 @@ int main(int argc, char * argv[])
     rclcpp::shutdown();
     return 0;
 }
+
