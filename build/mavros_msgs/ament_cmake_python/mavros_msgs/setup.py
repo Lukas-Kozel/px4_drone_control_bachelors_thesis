2034eef5f7361c6cfb1e1203de496457de0b42ea
Lukas-Kozel <l.kozel@outlook.cz>
2024-01-30 11:38:36 +0100

    removed ignored dirs

--- build/mavros_msgs/ament_cmake_python/mavros_msgs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='mavros_msgs',
-    version='2.6.0',
-    packages=find_packages(
-        include=('mavros_msgs', 'mavros_msgs.*')),
-)
